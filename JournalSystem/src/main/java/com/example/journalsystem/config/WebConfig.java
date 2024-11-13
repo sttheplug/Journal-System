e64bd3ab3d5a1ba21f54c83f8777001f313864be
AlaaAbdulrazzaq <Alaaabdulrazzaq3@gmail.com>
2024-11-13 15:43:53 +0100

    Login / Register
    Redirection to login fixed, login debugged

--- JournalSystem/src/main/java/com/example/journalsystem/config/WebConfig.java
+++ JournalSystem/src/main/java/com/example/journalsystem/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
